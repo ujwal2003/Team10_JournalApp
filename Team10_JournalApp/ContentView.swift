9a5dd938bc2b16f687e59746d86fcdf5638c7622
ujwal2003 <joshiujwal2003@gmail.com>
2024-10-06 17:54:21 -0500

    Initial Commit

---
+++ Team10_JournalApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Team10_JournalApp
+//
+//  Created by ujwal joshi on 10/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Team10_JournalApp/Team10_JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team10_JournalAppApp.swift
+//  Team10_JournalApp
+//
+//  Created by ujwal joshi on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Team10_JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
