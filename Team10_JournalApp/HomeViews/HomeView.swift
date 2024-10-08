a87b105348f40e4caaac4a2b4e2ef82f0ebfb695
ujwal2003 <joshiujwal2003@gmail.com>
2024-10-08 13:36:53 -0500

    HomeView initial setup

---
+++ Team10_JournalApp/HomeViews/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Team10_JournalApp
+//
+//  Created by ujwal joshi on 10/8/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, Home View!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
