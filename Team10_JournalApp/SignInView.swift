14d71c86148665a67592a4e5e59a3e6ff7ce8959
aaurtaza <alexurtaza12@gmail.com>
2024-10-15 16:52:16 -0500

    Create SplashScreenView

---
+++ Team10_JournalApp/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  Team10_JournalApp
+//
+//  Created by Alvaro on 10/8/24.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
