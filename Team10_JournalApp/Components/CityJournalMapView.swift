be6d54885d81a97f7b6d71ecfe5d785e557002bc
ujwal2003 <joshiujwal2003@gmail.com>
2024-10-19 15:50:29 -0500

    maps and buildings

--- Team10_JournalApp/Components/CityJournalMapView.swift
+++ Team10_JournalApp/Components/CityJournalMapView.swift
@@ -7,28 +7,62 @@
 
 import SwiftUI
 
-struct CityJournalMapView: View {
+//! Magic Number
+let isIphone16ProMaxPortrait: Bool = UIScreen.main.bounds.height == 956.0
+
+enum Map: String {
+    case Map1 = "Map_1"
+    case Map2 = "Map_2"
+    case Map3 = "Map_3"
+    case Map4 = "Map_4"
+}
+
+enum Building: String {
+    case BlueTower = "blue_building"
+    case BrownTower = "brown_building"
+}
+
+struct BuildingView: View {
+    var geometry: GeometryProxy
+    var xCoord: CGFloat
+    var yCoord: CGFloat
+    var onClick: () -> Void
     
-    //! Magic Number
-    let isIphone16ProMaxPortrait: Bool = UIScreen.main.bounds.height == 956.0
+    init(geometry: GeometryProxy, xCoord: CGFloat, yCoord: CGFloat, onClick: @escaping () -> Void) {
+        self.geometry = geometry
+        self.onClick = onClick
+        
+        self.xCoord = min(max(xCoord, 0), 1)
+        self.yCoord = min(max(yCoord, 0), 1)
+    }
+    
+    var body: some View {
+        Button(action: { onClick() }, label: {
+            Image("blue_building")
+                .resizable()
+                .scaledToFit()
+                .frame(width: geometry.size.width * 1,
+                       height: geometry.size.height * 0.16)
+        })
+        .position(x: geometry.size.width * xCoord, y: geometry.size.height * yCoord)
+    }
+}
+
+struct CityJournalMapView: View {
+    var map: Map = .Map1
     
     var body: some View {
         ZStack {
             GeometryReader { geometry in
-                Image("Map_1")
+                Image(map.rawValue)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: geometry.size.width)
                     .scaleEffect(x: isIphone16ProMaxPortrait ? 1 : 1.13)
                 
-                Button(action: { print("green building clicked!") }, label: {
-                    Image(systemName: "building.fill")
-                        .resizable()
-                        .frame(width: geometry.size.width * 0.1,
-                               height: geometry.size.height * 0.15)
-                        .foregroundStyle(Color.green)
-                })
-                .position(x: geometry.size.width * 0.88, y: geometry.size.height * 0.15)
+                BuildingView(geometry: geometry,
+                             xCoord: 0.88, yCoord: 0.15,
+                             onClick: { print("hiiiii!") })
                 
                 Button(action: { print("cyan building clicked") }, label: {
                     Image(systemName: "building.fill")
