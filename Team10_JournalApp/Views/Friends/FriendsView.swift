626ca930fbd1f02ea4a0664e9409a00e64716b97
ujwal2003 <joshiujwal2003@gmail.com>
2024-10-31 15:21:36 -0500

    feat: refactored friends view and friends list row components

--- Team10_JournalApp/Views/Friends/FriendsView.swift
+++ Team10_JournalApp/Views/Friends/FriendsView.swift
@@ -7,43 +7,6 @@
 
 import SwiftUI
 
-struct HeadingToolBarView: View {
-    var contentTitle: String
-    var isEditVisible: Bool
-    var isEditing: Bool
-    
-    var onEditClick: () -> Void
-    var onAddIconClick: () -> Void
-    
-    var body: some View {
-        HStack(alignment: .center) {
-            Text(contentTitle)
-                .font(.system(size: 24))
-                .fontWeight(.semibold)
-            
-            Spacer()
-            
-            if isEditVisible {
-                Button(action: { onEditClick() }) {
-                    Text(isEditing ? "Done" : "Edit")
-                        .font(.system(size: 20))
-                        .fontWeight(.medium)
-                        .padding(.horizontal, 8)
-                }
-            }
-            
-            Button(action: { onAddIconClick() }) {
-                Image(systemName: "person.crop.circle.badge.plus")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 30)
-            }
-        }
-        .padding()
-        .padding(.horizontal)
-    }
-}
-
 struct FriendsView: View {
     @StateObject var viewModel = FriendsViewModel()
     @Environment(\.editMode) var editMode
@@ -52,10 +15,18 @@ struct FriendsView: View {
     
     var body: some View {
         NavigationStack {
-            DefaultRectContainer(title: .init(text: "Friends", fontSize: 30),
-                                 subtitle: .init(text: "", fontSize: 20)) {
+            AppLayoutContainer(height: 0.0) {
+                VStack {
+                    Text("Friends")
+                        .font(.system(size: 30.0).weight(.heavy))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 16)
+                        .foregroundStyle(Color.black)
+                }
+                .padding()
                 
-                VStack(spacing: 0.0) {
+            } containerContent: {
+                VStack {
                     Picker("", selection: $viewModel.selectedContent) {
                         Text("Friends").tag(FriendSelectionContent.Friends)
                         Text("Requests").tag(FriendSelectionContent.Requests)
@@ -65,85 +36,27 @@ struct FriendsView: View {
                     .padding()
                     
                     SearchBarView(searchText: $viewModel.searchQuery)
-                }
-                
-                
-                HeadingToolBarView(contentTitle: viewModel.selectedContent.contentTitle,
-                                   isEditVisible: viewModel.isEditButtonVisible(),
-                                   isEditing: isEditing,
-                                   onEditClick: { isEditing.toggle() },
-                                   onAddIconClick: { viewModel.isAddFriendSheetVisible.toggle() })
-                .sheet(isPresented: $viewModel.isAddFriendSheetVisible) {
-                    AddFriendView(friendsViewModel: viewModel)
-                }
-                
-                //MARK: - Friends List
-                ScrollView {
-                    VStack(spacing: 15.0) {
-                        if viewModel.selectedContent == .Friends {
-                            ForEach(viewModel.getFilteredFriends(), id: \.self) { friend in
-                                if isEditing {
-                                    HStack {
-                                        Button(action: {
-                                            Task {
-                                                await viewModel.removeFriend(friend: friend)
-                                            }
-                                        }) {
-                                            Image(systemName: "minus.circle.fill")
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 25)
-                                                .foregroundStyle(Color.red)
-                                        }
-                                        .padding(.horizontal, 8)
-                                        
-                                        FriendCheckInRow(friendName: friend, isCheckInVisible: false)
-                                    }
-                                } else {
-                                    NavigationLink(destination: FriendCheckInView(friendName: friend, friendsViewModel: viewModel)) {
-                                        FriendCheckInRow(friendName: friend)
-                                    }
-                                }
-                            }
-                            
-                        } else if viewModel.selectedContent == .Requests {
-                            ForEach(viewModel.getFilteredRequests(), id: \.self) { request in
-                                FriendRequestRow(name: request,
-                                                 onAcceptClick: {
-                                                     Task {
-                                                        await viewModel.acceptFriendRequest(username: request)
-                                                     }
-                                                   },
-                                                 onRejectClick: {
-                                                     Task {
-                                                        await viewModel.rejectFriendRequest(username: request)
-                                                     }
-                                                   }
-                                                )
-                            }
-                            
-                        } else {
-                            ForEach(viewModel.getFilteredInvitations(), id: \.self) { invite in
-                                FriendInviteRow(name: invite,
-                                                onDismissClick: {
-                                                    Task {
-                                                        await viewModel.revokeFriendInvite(username: invite)
-                                                    }
-                                                })
-                            }
-                        }
-                        
+                    
+                    HeadingToolBarView(contentTitle: viewModel.selectedContent.contentTitle,
+                                       isEditVisible: viewModel.isEditButtonVisible(),
+                                       isEditing: isEditing,
+                                       onEditClick: { isEditing.toggle() },
+                                       onAddIconClick: { viewModel.isAddFriendSheetVisible.toggle() })
+                    .sheet(isPresented: $viewModel.isAddFriendSheetVisible) {
+                        AddFriendView(friendsViewModel: viewModel)
                     }
+                    
+                    List {
+                        ForEach(viewModel.getFilteredFriends(), id: \.self) { friend in
+                            FriendListRowView(itemName: friend,
+                                              itemContent: .emptyContent
+                            )
+                        }
+                    }
+                    .listStyle(.plain)
+                    
                 }
-//                .navigationDestination(for: String.self) { friend in
-//                    FriendCheckInView(friendName: friend, friendsViewModel: viewModel)
-//                }
-                
             }
         }
     }
-}
-
-#Preview {
-    FriendsView()
 }
