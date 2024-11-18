859718e5761ea14ba1af3261607f0e697cddef83
ujwal2003 <joshiujwal2003@gmail.com>
2024-11-18 16:30:01 -0600

    refactored growth report view as own component

---
+++ Team10_JournalApp/Views/Home/Subviews/GrowthReportView.swift
@@ -0,0 +1,40 @@
+struct GrowthReportView: View {
+    var geometry: GeometryProxy
+    
+    var title: String
+    var text: String
+    var sentiment: Sentiment
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0.0) {
+            Text(title)
+                .font(.system(size: 20))
+                .fontWeight(.medium)
+                .padding(.horizontal)
+            
+            HStack {
+                Text("Sentiment:")
+                    .font(.system(size: 16))
+                    .fontWeight(.medium)
+                
+                sentiment.textView
+            }
+            .padding(.horizontal)
+            
+            ScrollView {
+                Text(text)
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .frame(height: geometry.size.height * 0.21)
+            .background(Color.hex("#DDF2FD"))
+            .clipShape(RoundedCorner(radius: 5))
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                .inset(by: 0.5)
+                .stroke(Color(red: 0.26, green: 0.49, blue: 0.62).opacity(0.8), lineWidth: 1)
+            )
+            .padding()
+        }
+    }
+}
