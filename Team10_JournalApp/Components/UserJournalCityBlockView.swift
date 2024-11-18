6a6bbc925a7f38d210500bad194a7ed3b9c5fea8
ujwal2003 <joshiujwal2003@gmail.com>
2024-11-17 18:12:35 -0600

    building + sign component

--- Team10_JournalApp/Components/UserJournalCityBlockView.swift
+++ Team10_JournalApp/Components/UserJournalCityBlockView.swift
@@ -7,6 +7,52 @@
 
 import SwiftUI
 
+struct CityBuildingView: View {
+    var id = UUID()
+    
+    var building: Building
+    var buildingHeight: CGFloat
+    
+    var buildingNameSign: BuildingNameSign
+    var buildingSignHeight: CGFloat
+    
+    var coords: (x: CGFloat, y: CGFloat)
+    var onClick: () -> Void
+    
+    var body: some View {
+        Button(action: onClick) {
+            if buildingNameSign.category == .Billboard {
+                VStack(spacing: -20) {
+                    Image(buildingNameSign.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: buildingSignHeight)
+                    
+                    Image(building.rawValue)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: buildingHeight)
+                }
+                
+            } else if buildingNameSign.category == .Sign {
+                HStack(spacing: -10) {
+                    Image(building.rawValue)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: buildingHeight)
+                    
+                    Image(buildingNameSign.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: buildingSignHeight)
+                }
+            }
+        }
+        .position(x: coords.x, y: coords.y)
+        
+    }
+}
+
 struct UserJournalCityBlockView: View {
     var body: some View {
         GeometryReader { geometry in
@@ -17,23 +63,17 @@ struct UserJournalCityBlockView: View {
                     .padding(4)
                     .frame(width: geometry.size.width, alignment: .center)
                 
-                Button(action: {}) {
-                    VStack(spacing: -20) {
-                        Image("SundayBillboard")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 60)
-                        
-                        Image("light_blue_building")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: geometry.size.height * 0.18)
-                    }
-                    
-                }
-                .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.8)
+                CityBuildingView(
+                    building: .RedRuin,
+                    buildingHeight: geometry.size.height * 0.18,
+                    buildingNameSign: .SundaySign,
+                    buildingSignHeight: 60,
+                    coords: (x: geometry.size.width * 0.5, y: geometry.size.height * 0.8),
+                    onClick: {}
+                )
                 
             }
         }
+        
     }
 }
