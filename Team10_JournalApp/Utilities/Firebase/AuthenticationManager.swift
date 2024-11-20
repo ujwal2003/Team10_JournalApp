650b831bc7e21745f0066a4fcc46ab0c4833fc81
ujwal2003 <joshiujwal2003@gmail.com>
2024-11-19 23:35:56 -0600

    moved auth result data model to models folder

--- Team10_JournalApp/Utilities/Firebase/AuthenticationManager.swift
+++ Team10_JournalApp/Utilities/Firebase/AuthenticationManager.swift
@@ -8,17 +8,7 @@
 import Foundation
 import FirebaseAuth
 
-struct AuthDataResultModel {
-    let uid: String
-    let email: String?
-    let photoUrl: String?
-    
-    init(user: User) {
-        self.uid = user.uid
-        self.email = user.email
-        self.photoUrl = user.photoURL?.absoluteString
-    }
-}
+
 
 final class AuthenticationManager {
     static let shared = AuthenticationManager()
