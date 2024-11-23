5448b4c3edd29434b53b8eb75c9721cfa54922bf
ujwal2003 <joshiujwal2003@gmail.com>
2024-11-23 12:48:17 -0600

    feat: user manager get & create functions

--- Team10_JournalApp/Utilities/Firebase/UserManager.swift
+++ Team10_JournalApp/Utilities/Firebase/UserManager.swift
@@ -17,4 +17,16 @@ final class UserManager {
     private func userDocument(userId: String) -> DocumentReference {
         userCollection.document(userId)
     }
+    
+    func getUser(userId: String) async throws -> UserProfile {
+        try await userDocument(userId: userId).getDocument(as: UserProfile.self)
+    }
+    
+    func createNewUser(user: UserProfile) async throws {
+        try userDocument(userId: user.userId).setData(from: user, merge: false)
+    }
+    
+    // TODO: update user display name
+    
+    // TODO: update user friends list
 }
