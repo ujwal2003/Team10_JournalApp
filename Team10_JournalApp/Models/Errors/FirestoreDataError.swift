25f75984a8a556fcaf7ba03e503ee5ca4302c7e7
ujwal2003 <joshiujwal2003@gmail.com>
2024-11-25 14:48:50 -0600

    creat and fetch city block data

--- Team10_JournalApp/Models/Errors/FirestoreDataError.swift
+++ Team10_JournalApp/Models/Errors/FirestoreDataError.swift
@@ -9,13 +9,17 @@ import Foundation
 
 enum FirestoreDataError: Error {
     case journalEntryNotFound(userID: String, date: Date)
+    case cityBlockMapNotFound(userID: String, startDate: Date, endDate: Date)
 }
 
 extension FirestoreDataError: LocalizedError {
     var errorDescription: String? {
         switch self {
-        case .journalEntryNotFound(let userID, let date):
-            return "Could not find journal entry for user \(userID) on date \(date)"
+            case .journalEntryNotFound(let userID, let date):
+                return "Could not find journal entry for user \(userID) on date \(date)"
+                
+            case .cityBlockMapNotFound(let userID, let startDate, let endDate):
+                return "Could not find map data for user \(userID) in the week of: \(startDate) - \(endDate)"
         }
     }
 }
--- Team10_JournalApp/Utilities/Firebase/CityBlockManager.swift
+++ Team10_JournalApp/Utilities/Firebase/CityBlockManager.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+final class CityBlockManager {
+    static let shared = CityBlockManager()
+    private init() { }
+    
+    private let cityCollection = Firestore.firestore().collection("cities")
+    
+    func createNewCityBlockMap(cityBlockData: CityBlockData) async throws {
+        try cityCollection.addDocument(from: cityBlockData)
+    }
+    
+    private func cityBlockMapDocument(userId: String, weekStartDate: Date, weekEndDate: Date) async throws -> DocumentReference {
+        let calendar = Calendar.current
+        let normalizedWeekStartDate = calendar.startOfDay(for: weekStartDate)
+        let normalizedWeekEndDate = calendar.startOfDay(for: weekEndDate)
+        
+        let queryWeekEndRangeDate = calendar.date(byAdding: .day, value: 1, to: normalizedWeekEndDate)!
+        
+        let query = cityCollection
+            .whereField("user_id", isEqualTo: userId)
+            .whereField("week_start_date", isGreaterThanOrEqualTo: normalizedWeekStartDate)
+            .whereField("week_end_date", isLessThan: queryWeekEndRangeDate)
+            .limit(to: 1)
+        
+        let querySnapshot = try await query.getDocuments()
+        guard let document = querySnapshot.documents.first else {
+            throw FirestoreDataError.cityBlockMapNotFound(userID: userId, startDate: weekStartDate, endDate: weekEndDate)
+        }
+        
+        return document.reference
+    }
+    
+    func getCityBlockData(userId: String, weekStartDate: Date, weekEndDate: Date) async throws -> CityBlockData {
+        try await cityBlockMapDocument(userId: userId, weekStartDate: weekStartDate, weekEndDate: weekEndDate).getDocument(as: CityBlockData.self)
+    }
+}
