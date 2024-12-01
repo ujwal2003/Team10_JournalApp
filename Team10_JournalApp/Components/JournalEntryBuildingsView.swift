2b1b19ccdc0ff566a7e2d70b04c52c3dc5b80158
ujwal2003 <joshiujwal2003@gmail.com>
2024-12-01 15:10:23 -0600

    feat: basic template for new city journal building component

---
+++ Team10_JournalApp/Components/JournalEntryBuildingsView.swift
@@ -0,0 +1,113 @@
+//
+//  JournalEntryBuildingsView.swift
+//  Team10_JournalApp
+//
+//  Created by ujwal joshi on 12/1/24.
+//
+
+import SwiftUI
+
+struct JournalEntryBuildingsView: View {
+    @Environment(\.dismiss) var dismiss
+    @State var selectedMenuView: CityBuildingViewSelection
+    
+    @State var building: Building
+    @State var date: Date
+    @State var journalID: String
+    
+    @State var fetchedJournalEntry: JournalEntry?
+    
+    @StateObject var viewModel = CityJournalBuildingViewModel()
+    
+    init(
+        selectedMenuView: CityBuildingViewSelection = .Sentiment,
+        building: Building,
+        date: Date,
+        journalID: String
+    ) {
+        self.selectedMenuView = selectedMenuView
+        self.building = building
+        self.date = date
+        self.journalID = journalID
+        self.fetchedJournalEntry = nil
+    }
+    
+    var body: some View {
+        NavigationStack {
+            GeometryReader { geometry in
+                ScrollView {
+                    VStack {
+                        
+                        VStack(spacing: 0) {
+                            Text("Title")
+                                .font(.system(size: 30))
+                                .fontWeight(.heavy)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .padding()
+                            
+                            Picker("Selection", selection: $selectedMenuView) {
+                                Text("Sentiment").tag(CityBuildingViewSelection.Sentiment)
+                                Text("Journal").tag(CityBuildingViewSelection.Journal)
+                            }
+                            .pickerStyle(.segmented)
+                            .padding([.leading, .bottom, .trailing])
+                        }
+                        
+                        if self.building.category != .Building && self.building != .Scaffolding {
+                            viewModel.getConstructionOrRuinIndicatorsView(building: building)
+                            
+                        } else if self.building == .Scaffolding {
+                            viewModel.getScaffoldingIndicatorsView()
+                            
+                        } else {
+                            GrowthReportView(
+                                geometry: geometry,
+                                title: "Title",
+                                text: "Lorem ipsum dolor",
+                                sentiment: .Concerning
+                            )
+                            
+                            GrowthReportView(
+                                geometry: geometry,
+                                title: "Title",
+                                text: "Lorem ipsum dolor",
+                                sentiment: .Concerning
+                            )
+                            
+                            GrowthReportView(
+                                geometry: geometry,
+                                title: "Title",
+                                text: "Lorem ipsum dolor",
+                                sentiment: .Concerning
+                            )
+                        }
+                    }
+                    
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Done")
+                          .font(.custom("SF Pro", size: 16).weight(.semibold))
+                          .multilineTextAlignment(.center)
+                          .foregroundColor(Color(red: 0, green: 0.48, blue: 1))
+                    }
+
+                }
+            }
+            
+        }
+        
+    }
+}
+
+#Preview {
+    JournalEntryBuildingsView(
+        building: .Scaffolding,
+        date: Date(),
+        journalID: ""
+    )
+}
---
+++ Team10_JournalApp/ViewModels/Journal/CityJournalBuildingViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  CityJournalBuildingViewModel.swift
+//  Team10_JournalApp
+//
+//  Created by ujwal joshi on 12/1/24.
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor
+final class CityJournalBuildingViewModel: ObservableObject {
+    func getConstructionOrRuinIndicatorsView(building: Building) -> some View {
+        let indicatorIcon = (building.category == .Construction) ? "hugeicons_question" : "subway_error"
+        let iconSize: CGFloat = (building.category == .Construction) ? 300 : 245
+        
+        var indicatorText: String {
+            if building.category == .Ruin {
+                return "Not available. These buildings lay in ruins because no journal entries were made on this day."
+            }
+            
+            return "The journal entries have yet to be made, these buildings are still under construction."
+        }
+        
+        return VStack {
+            Image(indicatorIcon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: iconSize)
+                .padding()
+            
+            Text(indicatorText)
+                .font(.system(size: 22))
+                .fontWeight(.light)
+                .multilineTextAlignment(.center)
+                .padding()
+        }
+    }
+    
+    func getScaffoldingIndicatorsView() -> some View {
+        return VStack {
+            HStack {
+                Spacer()
+                Image(systemName: "book.and.wrench")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.hex("#5B5C69").opacity(0.70))
+                Spacer()
+            }
+            
+            Text("This building is set to be constructed at a future date.")
+                .font(.system(size: 22))
+                .multilineTextAlignment(.center)
+                .lineLimit(nil)
+                .fixedSize(horizontal: false, vertical: true)
+                .padding()
+        }
+    }
+    
+}
