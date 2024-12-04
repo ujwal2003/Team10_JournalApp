b4ff88b62584bdc7c186e65104863b8ef9fabeef
ujwal2003 <joshiujwal2003@gmail.com>
2024-12-04 00:40:32 -0600

    places data models

---
+++ Team10_JournalApp/Models/ReccomendedActionsLocationsModels.swift
@@ -0,0 +1,41 @@
+//
+//  ReccomendedActionsLocationsModels.swift
+//  Team10_JournalApp
+//
+//  Created by ujwal joshi on 12/4/24.
+//
+
+import Foundation
+
+enum CreativeSpaces: String {
+    case ArtStudios = "Art Studios"
+    case Makerspaces = "Makerspaces"
+    case ScenicViews = "Scenic Views"
+    case Libraries = "Libraries"
+    case Music = "Music"
+}
+
+enum EnjoyableExperiences: String {
+    case Parks = "Parks & Gardens"
+    case ThemeParks = "Theme Parks"
+    case Museums = "Museums"
+    case Cafes = "Cafes"
+    case Theatres = "Theatres"
+}
+
+enum InteractiveVenues: String {
+    case EscapeRooms = "Escape Rooms"
+    case CookingClasses = "Cooking Classes"
+    case DanceStudios = "Dance Studios"
+    case Karaoke = "Karaoke"
+    case CommunityGardens = "Community Gardens"
+    case Swimming = "Swimming"
+}
+
+enum SereneEnvironments: String {
+    case Trails = "Nature Trails"
+    case Hiking = "Hiking Trails"
+    case Beaches = "Beaches"
+    case Aquariums = "Aquariums"
+    case Planetariums = "Planetariums"
+}
