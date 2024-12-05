bb4933b6c2aeab3b47df90a82a8baf92e8594ff6
ujwal2003 <joshiujwal2003@gmail.com>
2024-12-04 23:24:37 -0600

    landscape map

--- Team10_JournalApp/Views/Home/HomeLandscapeView.swift
+++ Team10_JournalApp/Views/Home/HomeLandscapeView.swift
@@ -14,59 +14,50 @@ struct HomeLandscapeView: View {
     @StateObject var viewModel = HomeViewModel()
     
     var body: some View {
-        AppLayoutContainer(height: 2.0) {
-            VStack(spacing: 0.0) {
-                Text("CatchUp")
-                    .font(.system(size: 30.0))
-                    .fontWeight(.heavy)
-                    .padding([.top, .leading, .trailing])
-                    .padding(.leading)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                Group {
-                    if let userProfile = appController.loadedUserProfile {
-                        Text("Welcome, \(userProfile.displayName)")
-                    } else {
-                        Text("Loading...")
-                    }
-                }
-                .font(.system(size: 20.0))
-                .fontWeight(.light)
-                .padding([.leading, .trailing, .bottom])
-                .padding(.leading)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
+        NavigationStack {
             
-        } containerContent: {
-            VStack {
-                
-                ZStack {
-                    Image(Map.Map1.rawValue)
-                        .resizable()
-                        .scaledToFit()
-                        .padding(5)
+            AppLayoutContainer(height: 2.0) {
+                VStack(spacing: 0.0) {
+                    Text("CatchUp")
+                        .font(.system(size: 30.0))
+                        .fontWeight(.heavy)
+                        .padding([.top, .leading, .trailing])
+                        .padding(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     
-                    Button {
-                        
-                    } label: {
-                        VStack {
-                            Image(systemName: "7.circle.fill")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50)
-                                .foregroundStyle(Color.yellow)
-                                .shadow(radius: 5)
-                            
-                            HStack(spacing: 0) {
-                                Image(Building.PurpleConstruction.rawValue)
-                                Image(Building.LightBrownTower.rawValue)
-                                Image(Building.GreenRuin.rawValue)
-                            }
+                    Group {
+                        if let userProfile = appController.loadedUserProfile {
+                            Text("Welcome, \(userProfile.displayName)")
+                        } else {
+                            Text("Loading...")
                         }
                     }
+                    .font(.system(size: 20.0))
+                    .fontWeight(.light)
+                    .padding([.leading, .trailing, .bottom])
+                    .padding(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 }
                 
+            } containerContent: {
+                VStack {
+                    
+                    LandscapeJournalMapView(
+                        map: .Map1,
+                        buildings: [
+                            .init(style: .BlueConstruction, onClick: {}),
+                            .init(style: .BlueConstruction, onClick: {}),
+                            .init(style: .BlueConstruction, onClick: {}),
+                            .init(style: .BlueConstruction, onClick: {}),
+                            .init(style: .BlueConstruction, onClick: {}),
+                            .init(style: .BlueConstruction, onClick: {}),
+                            .init(style: .BlueConstruction, onClick: {})
+                        ]
+                    )
+                    
+                }
             }
+            
         }
     }
 }
